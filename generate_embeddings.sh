ae47d2afa52a5f567ede8460e56fd9304b292c04
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-02-23 14:14:16 +0000

    Updated: generate_embeddings.sh

--- generate_embeddings.sh
+++ generate_embeddings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH -p titanxp_12GB                     # Partition
+#SBATCH -p gtx1080_8GB                      # Partition
 #SBATCH --job-name=omad_aut                 # Job name
 #SBATCH -o slurm.%N.%j.out                  # STDOUT
 #SBATCH -e slurm.%N.%j.err                  # STDERR
@@ -10,6 +10,8 @@
 echo "OrthoMAD v2 - Generate Bonafide Embeddings | Started | Training"
 
 
-python code/inference_autoencoder.py --data_dir /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/SMDD/os25k_bf_t_cropped --data_dir /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/SMDD/os25k_m_t_cropped --new_data_dir /nas-ctm01/datasets/public/BIOMETRICS/embeddings --dataset BonaFideImages --model UNetAutoencoder --checkpoint /nas-ctm01/homes/tgoncalv/OrthoMAD/results/bonafideimages/unetautoencoder/2023-02-17_10-14-52/weights/unetautoencoder_bonafideimages_best.pt
+# python code/inference_autoencoder.py --data_dir /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/SMDD/os25k_bf_t_cropped --data_dir /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/SMDD/os25k_m_t_cropped --new_data_dir /nas-ctm01/datasets/public/BIOMETRICS/embeddings --dataset BonaFideImages --model UNetAutoencoder --checkpoint /nas-ctm01/homes/tgoncalv/OrthoMAD/results/bonafideimages/unetautoencoder/2023-02-17_10-14-52/weights/unetautoencoder_bonafideimages_best.pt
+python code/inference_autoencoder.py --data_dir /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/SMDD/os25k_bf_t_cropped --data_dir /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/SMDD/os25k_m_t_cropped --new_data_dir /nas-ctm01/datasets/public/BIOMETRICS/embeddings/unetautoencoder/2023-02-20_15-54-40 --dataset BonaFideImages --model UNetAutoencoder --checkpoint /nas-ctm01/homes/tgoncalv/OrthoMAD/results/bonafideimages/unetautoencoder/2023-02-20_15-54-40/weights/unetautoencoder_bonafideimages_best.pt
+
 
 echo "OrthoMAD v2 - Generate Bonafide Embeddings | Started | Training"
