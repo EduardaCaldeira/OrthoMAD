ac931bebf79d4bc1a724b1b6a058c1b5f727701f
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2023-03-02 20:09:23 +0000

    Updated: tiago_train_bce.sh

--- tiago_train_bce.sh
+++ tiago_train_bce.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #
-#SBATCH --partition=gtx1080ti_11GB    
-#SBATCH --output=train_BCE_SMDD_%N_%j              
+#SBATCH -p gtx1080ti_11GB                   # Partition
+#SBATCH --job-name=omdbce                   # Job name
+#SBATCH -o slurm.%N.%j.out                  # STDOUT
+#SBATCH -e slurm.%N.%j.err                  # STDERR             
 
-python3 code/train.py --train_csv_path="../../datasets/public/BIOMETRICS/Face_Morphing/SMDD/train.csv" --max_epoch=250 --batch_size=16 --latent_size=256 --lr=0.00001 --weight_loss=100 --loss_measure=bce --is_test=False --model_path=trained_bce_SMDD_min --output_dir=output_bce_min
+# MULT
+python code/train.py --train_csv_path /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/SMDD/train.csv --model_name resnet18_mult --max_epoch 250 --batch_size 16 --latent_size 128 --lr 0.00001 --weight_loss 100 --loss_measure bce --is_test False --model_path /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_bce_SMDD_mult/resnet18_mult_w100/resnet18_mult_w100.pth --output_dir /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_bce_SMDD_mult/resnet18_mult_w100
+
+
+# MIN
+python code/train.py --train_csv_path /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/SMDD/train.csv --model_name resnet18_min --max_epoch 250 --batch_size 16 --latent_size 128 --lr 0.00001 --weight_loss 100 --loss_measure bce --is_test False --model_path /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_bce_SMDD_min/resnet18_min_w100/resnet18_min_w100.pth --output_dir /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_bce_SMDD_min/resnet18_min_w100
