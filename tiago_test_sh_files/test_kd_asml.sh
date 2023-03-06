6045ae4386a1156b41c7deeba6cd234734cf9bcb
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-03-06 16:19:03 +0000

    Updated test files.

--- tiago_test_sh_files/test_kd_asml.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --partition=gpu          
-#SBATCH --output=test_kd_asml_mult_%N_%j              
-
-python3 code/train.py --test_csv_path="../../datasets/public/BIOMETRICS/Face_Morphing/facelab_london/asml.csv" --latent_size=256 --batch_size=16 --is_train=False --model_path=trained_kd_SMDD_mult --output_dir=output_kd_mult
