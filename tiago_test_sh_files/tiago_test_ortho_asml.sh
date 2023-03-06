1fa75e429e3c02cad019063d72fc2e5ec8e59ddf
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-03-06 10:53:21 +0000

    Updated test files.

--- tiago_test_sh_files/tiago_test_ortho_asml.sh
+++ tiago_test_sh_files/tiago_test_ortho_asml.sh
@@ -13,16 +13,16 @@
 
 # MULT
 # EMB SIZE 128
-python code/train.py --test_csv_path /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/facelab_london/asml.csv --model_name resnet18_mult --max_epoch 250 --batch_size 16 --latent_size 128 --lr 0.00001 --weight_loss 100 --loss_measure ortho --is_train False --model_path /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_mult/resnet18_mult_w100/resnet18_mult_w100.pth --output_dir /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_mult/resnet18_mult_w100
+# python code/train.py --test_csv_path /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/facelab_london/asml.csv --model_name resnet18_mult --max_epoch 250 --batch_size 16 --latent_size 128 --lr 0.00001 --weight_loss 100 --loss_measure ortho --is_train False --model_path /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_mult/resnet18_mult_w100/resnet18_mult_w100.pth --output_dir /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_mult/resnet18_mult_w100
 
 # EMB SIZE 64
-python code/train.py --test_csv_path /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/facelab_london/asml.csv --model_name resnet18_mult --max_epoch 250 --batch_size 16 --latent_size 64 --lr 0.00001 --weight_loss 100 --loss_measure ortho --is_train False --model_path /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_mult/resnet18_mult_emb64_w100/resnet18_mult_emb64_w100.pth --output_dir /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_mult/resnet18_mult_emb64_w100
+# python code/train.py --test_csv_path /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/facelab_london/asml.csv --model_name resnet18_mult --max_epoch 250 --batch_size 16 --latent_size 64 --lr 0.00001 --weight_loss 100 --loss_measure ortho --is_train False --model_path /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_mult/resnet18_mult_emb64_w100/resnet18_mult_emb64_w100.pth --output_dir /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_mult/resnet18_mult_emb64_w100
 
 
 
 # MIN
 # EMB 128
-python code/train.py --test_csv_path /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/facelab_london/asml.csv --model_name resnet18_min --max_epoch 250 --batch_size 16 --latent_size 128 --lr 0.00001 --weight_loss 100 --loss_measure ortho --is_train False --model_path /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_min/resnet18_min_w100/resnet18_min_w100.pth --output_dir /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_min/resnet18_min_w100
+# python code/train.py --test_csv_path /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/facelab_london/asml.csv --model_name resnet18_min --max_epoch 250 --batch_size 16 --latent_size 128 --lr 0.00001 --weight_loss 100 --loss_measure ortho --is_train False --model_path /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_min/resnet18_min_w100/resnet18_min_w100.pth --output_dir /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_min/resnet18_min_w100
 
 # EMB 64
-python code/train.py --test_csv_path /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/facelab_london/asml.csv --model_name resnet18_min --max_epoch 250 --batch_size 16 --latent_size 64 --lr 0.00001 --weight_loss 100 --loss_measure ortho --is_train False --model_path /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_min/resnet18_min_emb64_w100/resnet18_min_emb64_w100.pth --output_dir /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_min/resnet18_min_emb64_w100
+# python code/train.py --test_csv_path /nas-ctm01/datasets/public/BIOMETRICS/Face_Morphing/facelab_london/asml.csv --model_name resnet18_min --max_epoch 250 --batch_size 16 --latent_size 64 --lr 0.00001 --weight_loss 100 --loss_measure ortho --is_train False --model_path /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_min/resnet18_min_emb64_w100/resnet18_min_emb64_w100.pth --output_dir /nas-ctm01/homes/tgoncalv/OrthoMAD/results/trained_ortho_SMDD_min/resnet18_min_emb64_w100
